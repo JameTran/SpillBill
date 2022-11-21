5cf302129eafe3b145938986074638d0cf462763
Jame Tran <jamedhtran@gmail.com>
2022-11-20 22:00:56 -0500

    commiting temp profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,3 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom"
+import image from "../components/images/profilepic.jpg";
+
 export default function Profile() {
-    return <h1>START</h1>
+    return (
+        <>
+            <img src={image} alt="Avatar" />
+            <h1>Profile</h1>
+            <h3>Logan Brown</h3>
+            <h3>brownl33@mcmaster.ca</h3>
+            <h3>brown123</h3>
+        </> 
+        
+
+        
+
+    );
 }
